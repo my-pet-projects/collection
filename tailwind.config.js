bb9b3a2fec83a16b7d7b91d807b6d8177cf3da07
Gleb Filimonov <gleb.filimonov@sap.com>
2023-12-14 16:33:44 +0100

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["internal/component/*.templ"],
+  content: ["internal/component/*.templ", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
 };
