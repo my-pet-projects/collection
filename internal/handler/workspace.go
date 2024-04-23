55dae550cf81ff783997cbfd51d348a572ad0818
see-sharp-dev <>
2024-04-23 21:21:53 +0200

    add basic brewery update form

--- internal/handler/workspace.go
+++ internal/handler/workspace.go
@@ -5,7 +5,9 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
+	"strings"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 
 	"github.com/my-pet-projects/collection/internal/component/workspace"
@@ -54,8 +56,48 @@ func (h WorkspaceHandler) GetBreweryPage(ctx echo.Context) error {
 	}
 	page := workspace.NewPage(ctx, fmt.Sprintf("Edit Brewery - %s", brewery.Name))
 	breweryPage := workspace.BreweryPage{
-		Page:    page,
-		Brewery: brewery,
+		Page: page,
+		FormParams: workspace.BreweryFormParams{
+			Id:          brewery.Id,
+			Name:        brewery.Name,
+			CountryCode: brewery.CountryCode,
+			CityId:      brewery.GeoId,
+		},
 	}
 	return workspace.WorkspaceBreweryPage(breweryPage).Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
+func (h WorkspaceHandler) PostBreweryPage(ctx echo.Context) error {
+	idStr := ctx.FormValue("id")
+	id, parseErr := strconv.Atoi(idStr)
+	if parseErr != nil {
+
+	}
+	geoIdStr := ctx.FormValue("city")
+	geoId, parseErr := strconv.Atoi(geoIdStr)
+	if parseErr != nil {
+
+	}
+
+	formParams := workspace.BreweryFormParams{
+		Id:          id,
+		Name:        strings.TrimSpace(ctx.FormValue("name")),
+		CountryCode: ctx.FormValue("country"),
+		CityId:      geoId,
+	}
+
+	if formErrs, hasErrs := formParams.Validate(); hasErrs {
+		return render(ctx, workspace.BreweryForm(formParams, formErrs))
+	}
+
+	updErr := h.breweryService.UpdateBrewery(formParams.Id, formParams.Name, formParams.CityId)
+	if updErr != nil {
+		return render(ctx, workspace.BreweryForm(formParams, workspace.BreweryFormErrors{}))
+	}
+
+	return render(ctx, workspace.BreweryForm(formParams, workspace.BreweryFormErrors{}))
+}
+
+func render(ctx echo.Context, comp templ.Component) error {
+	return comp.Render(ctx.Request().Context(), ctx.Response().Writer)
 }
