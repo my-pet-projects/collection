1523f9804be899e23efe45f7aa8ad8025e182a40
Gleb Filimonov <gleb.filimonov@sap.com>
2024-11-20 19:06:38 +0100

    db layer clean-up

--- internal/db/beer_media.go
+++ internal/db/beer_media.go
@@ -21,41 +21,33 @@ func NewBeerMediaStore(db *DbClient, logger *slog.Logger) BeerMediaStore {
 	}
 }
 
-// func (s BeerMediaStore) GetMediaItem(ctx context.Context, mediaID int) (*model.BeerMedia, error) {
-// 	item := new(model.BeerMedia)
-// 	query := `SELECT id, media_id, beer_id, type
-// 			    FROM beer_medias
-// 		  	   WHERE media_id = ?`
-// 	resErr := s.db.QueryRow(query, mediaID).Scan(
-// 		&item.ID, &item.MediaID, &item.BeerID, &item.Type,
-// 	)
-// 	if resErr == sql.ErrNoRows {
-// 		return nil, nil
-// 	}
-// 	if resErr != nil {
-// 		return nil, errors.Wrap(resErr, "find existing beer media item")
-// 	}
-
-// 	return item, nil
-// }
-
 func (s BeerMediaStore) UpsertBeerMediaItem(ctx context.Context, mediaItem model.MediaItem, mediaImg *model.MediaImage, beerID *int) (model.BeerMedia, error) {
 	itemToUpsert := model.BeerMedia{
 		MediaID: mediaItem.ID,
 		Type:    mediaImg.ImageType,
 		BeerID:  beerID,
 	}
-	res := s.db.gorm.Clauses(
-		clause.OnConflict{
-			DoNothing: true,
-		},
-	).Table("beer_medias").Create(&itemToUpsert)
+	res := s.db.gorm.
+		Debug().
+		Clauses(
+			clause.OnConflict{
+				DoNothing: true,
+			},
+		).
+		Table("beer_medias").
+		Create(&itemToUpsert)
 
 	return itemToUpsert, res.Error
 }
 
 func (s BeerMediaStore) FetchMediaItems(ctx context.Context) ([]model.BeerMedia, error) {
 	var items []model.BeerMedia
-	result := s.db.gorm.Model(&model.BeerMedia{}).Joins("Media").Table("beer_medias").Find(&items)
+	result := s.db.gorm.
+		Debug().
+		Model(&model.BeerMedia{}).
+		Joins("Media").
+		Table("beer_medias").
+		Find(&items)
+
 	return items, result.Error
 }
--- internal/db/geography.go
+++ internal/db/geography.go
@@ -14,32 +14,6 @@ type GeographyStore struct {
 	logger *slog.Logger
 }
 
-// type Country struct {
-// 	Cca2         string
-// 	Cca3         string
-// 	Ccn3         *string
-// 	NameCommon   string
-// 	NameOfficial string
-// 	Region       string
-// 	Subregion    *string
-// 	FlagPng      string
-// }
-
-// type City struct {
-// 	Id               int
-// 	Name             string
-// 	AlternateNames   *string
-// 	CountryCode      string
-// 	Admin1Code       *string
-// 	Admin2Code       *string
-// 	Admin3Code       *string
-// 	Admin4Code       *string
-// 	ModificationDate string
-// 	Population       *int
-// 	Latitude         float64
-// 	Longitude        float64
-// }
-
 func NewGeographyStore(db *DbClient, logger *slog.Logger) GeographyStore {
 	return GeographyStore{
 		db:     db,
@@ -55,25 +29,6 @@ func (s GeographyStore) FetchCountries() ([]model.Country, error) {
 		Find(&items)
 
 	return items, result.Error
-
-	// query := "SELECT * FROM countries"
-	// res, resErr := s.db.Query(query)
-	// if resErr != nil || res.Err() != nil {
-	// 	return nil, errors.Wrap(resErr, "query countries")
-	// }
-	// defer res.Close() //nolint:errcheck
-
-	// countries := []Country{}
-	// for res.Next() {
-	// 	var country Country
-	// 	scanErr := res.Scan(&country.Cca2, &country.Cca3, &country.Ccn3, &country.NameCommon,
-	// 		&country.NameOfficial, &country.Region, &country.Subregion, &country.FlagPng)
-	// 	if scanErr != nil {
-	// 		return nil, errors.Wrap(scanErr, "scan query results")
-	// 	}
-	// 	countries = append(countries, country)
-	// }
-	// return countries, nil
 }
 
 func (s GeographyStore) FetchCitiesByCountry(countryCode string) ([]model.City, error) {
@@ -86,26 +41,4 @@ func (s GeographyStore) FetchCitiesByCountry(countryCode string) ([]model.City, 
 		Find(&items)
 
 	return items, result.Error
-	// query := `SELECT id, name, alternate_names, country_code,
-	// 				 admin1_code, admin2_code, admin3_code, admin4_code,
-	// 				 modification_date, population, latitude, longitude
-	// 		  FROM cities WHERE country_code = UPPER(?) LIMIT 100000`
-	// res, resErr := s.db.Query(query, countryCode)
-	// if resErr != nil || res.Err() != nil {
-	// 	return nil, errors.Wrap(resErr, "query cities")
-	// }
-	// defer res.Close() //nolint:errcheck
-
-	// cities := []City{}
-	// for res.Next() {
-	// 	var city City
-	// 	scanErr := res.Scan(&city.Id, &city.Name, &city.AlternateNames, &city.CountryCode,
-	// 		&city.Admin1Code, &city.Admin2Code, &city.Admin3Code, &city.Admin4Code,
-	// 		&city.ModificationDate, &city.Population, &city.Latitude, &city.Longitude)
-	// 	if scanErr != nil {
-	// 		return nil, errors.Wrap(scanErr, "scan query results")
-	// 	}
-	// 	cities = append(cities, city)
-	// }
-	// return cities, nil
 }
