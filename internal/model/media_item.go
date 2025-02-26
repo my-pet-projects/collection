83d162a5bc6b52541161c277525f130255d5d23f
see-sharp-dev <filimonovga.work@gmail.com>
2025-02-26 23:27:55 +0100

    patch: store image dimensions (#3)
    Co-authored-by: see-sharp-dev <see-sharp-dev@mail.com>

--- internal/model/media_item.go
+++ internal/model/media_item.go
@@ -5,11 +5,8 @@ import (
 	"crypto/md5" //nolint:gosec
 	"fmt"
 	"image"
-	"image/png"
-	"io"
 	"time"
 
-	"github.com/anthonynsimon/bild/transform"
 	"github.com/pkg/errors"
 )
 
@@ -34,28 +31,28 @@ type MediaMetadata struct {
 	Height int
 }
 
-type MediaContent struct {
-	Name        string
-	Bytes       []byte
-	Size        int
-	Metadata    MediaMetadata
-	ContentType string
-}
-
 type MediaItem struct {
 	ID               int `gorm:"primarykey"`
 	ExternalFilename string
 	OriginalFilename string
 	ContentType      string
 	Hash             string
+	Size             int
+	Width            int
+	Height           int
 	CreatedAt        time.Time
 	UpdatedAt        *time.Time
 }
 
 type MediaImage struct {
-	Original  MediaContent
-	Preview   MediaContent
-	ImageType BeerMediaType
+	OriginalName string
+	ExternalName string
+	Bytes        []byte
+	Hash         string
+	Size         int
+	Metadata     MediaMetadata
+	ContentType  string
+	ImageType    BeerMediaType
 }
 
 func NewMediaImage(formValues UploadFormValues) (*MediaImage, error) {
@@ -64,55 +61,28 @@ func NewMediaImage(formValues UploadFormValues) (*MediaImage, error) {
 		return nil, errors.Wrap(decodeErr, "decode image")
 	}
 
-	original := MediaContent{
-		Name:  fmt.Sprintf("original/%s", formValues.ExternalFilename()),
-		Bytes: formValues.Content,
-		Size:  len(formValues.Content),
-		Metadata: MediaMetadata{
-			Width:  image.Bounds().Dx(),
-			Height: image.Bounds().Dy(),
-		},
-		ContentType: formValues.ContentType,
+	imageMetadata := MediaMetadata{
+		Width:  image.Bounds().Dx(),
+		Height: image.Bounds().Dy(),
+	}
+	if imageMetadata.Width == 0 || imageMetadata.Height == 0 {
+		return nil, errors.New("invalid image dimensions")
 	}
 
-	beerMediaType, typeErr := NewBeerMediaType(original.Metadata)
+	beerMediaType, typeErr := NewBeerMediaType(imageMetadata)
 	if typeErr != nil {
 		return nil, errors.Wrap(typeErr, "unknown beer media type")
 	}
 
-	resizeRatio := 17
-	if beerMediaType == BeerMediaBottle {
-		resizeRatio = 3
-	} else if beerMediaType == BeerMediaCrownCap {
-		resizeRatio = 9
-	}
-
-	width := original.Metadata.Width / resizeRatio
-	height := original.Metadata.Height / resizeRatio
-	resized := transform.Resize(image, width, height, transform.Lanczos)
-
-	var previewBytes bytes.Buffer
-	writer := io.Writer(&previewBytes)
-	encodeErr := png.Encode(writer, resized)
-	if encodeErr != nil {
-		return nil, errors.Wrap(encodeErr, "encode preview image")
-	}
-
-	preview := MediaContent{
-		Name:  fmt.Sprintf("preview/%s", formValues.ExternalFilename()),
-		Bytes: previewBytes.Bytes(),
-		Size:  len(previewBytes.Bytes()),
-		Metadata: MediaMetadata{
-			Width:  width,
-			Height: height,
-		},
-		ContentType: formValues.ContentType,
-	}
-
 	mediaImage := &MediaImage{
-		Original:  original,
-		Preview:   preview,
-		ImageType: beerMediaType,
+		ExternalName: fmt.Sprintf("original/%s", formValues.ExternalFilename()),
+		Bytes:        formValues.Content,
+		Size:         len(formValues.Content),
+		Hash:         formValues.Hash(),
+		Metadata:     imageMetadata,
+		ContentType:  formValues.ContentType,
+		ImageType:    beerMediaType,
+		OriginalName: formValues.Filename,
 	}
 
 	return mediaImage, nil
