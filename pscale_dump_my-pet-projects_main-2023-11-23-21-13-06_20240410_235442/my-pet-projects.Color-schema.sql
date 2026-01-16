d0777513edbc1156cc3743e5eafae8e85768eaf0
see-sharp-dev <see-sharp-dev@mail.com>
2026-01-16 15:33:35 +0100

    update dumps

--- pscale_dump_my-pet-projects_main-2023-11-23-21-13-06_20240410_235442/my-pet-projects.Color-schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `Color` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- pscale_dump_my-pet-projects_main-2023-11-23-21-13-06_20240410_235442/my-pet-projects.TripDestinations-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `TripDestinations` (
-  `tripId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `countryId` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`tripId`,`countryId`),
-  UNIQUE KEY `TripDestinations_tripId_countryId_key` (`tripId`,`countryId`),
-  KEY `TripDestinations_tripId_idx` (`tripId`),
-  KEY `TripDestinations_countryId_idx` (`countryId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
